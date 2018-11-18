e05fa9d65cbfeb0aad437b565401982b83e282db
Jimmy Goddard <jimmy.goddard@gmail.com>
2018-11-18 16:46:28 -0500

    Finished homework for Wikipedia Search

---
+++ Wikipedia Search/server.R
@@ -0,0 +1,18 @@
+# Example: Shiny app that search Wikipedia web pages
+# File: server.R
+library(shiny)
+library(tm)
+library(stringi)
+library(proxy)
+library(wordcloud)
+source("WikiSearch.R")
+
+
+shinyServer(function (input, output) {
+  output$distPlot <- renderPlot({
+    result <- SearchWiki(input$select)
+    set.seed(123)
+    wordcloud(names(result), result, max.words = 50, colors = brewer.pal(6, "Dark2"))
+  })
+})
+
